d31fb66611303d8b9bf3d18aaa603b3674fe015a
Bill Bahr <78818242+billbahr@users.noreply.github.com>
2022-02-07 07:30:54 -0600

    Switch to flex, add some JS listeners

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,2 +1,18 @@
 const billAmount = document.querySelector("#bill-amount");
+const numPeople = document.querySelector("#num-people");
+const customTip = document.querySelector("#tip-custom");
+const tipButtons = document.querySelectorAll(".tip-selection>button");
 
+billAmount.addEventListener('change', (e) => {
+    console.log(e.target.value);
+});
+
+numPeople.addEventListener('change', (e) => {
+    console.log(e.target.value);
+});
+
+customTip.addEventListener('change', (e) => {
+    console.log(e.target.value);
+});
+
+tipButtons.forEach(button => console.log(button.innerText));
